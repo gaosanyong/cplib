9ab61c9a69be43605591ba65b2310a0a59dfdcbc
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-04-07 00:49:43 -0400

    convert tab to space

--- java/fenwick.java
+++ java/fenwick.java
@@ -1,22 +1,22 @@
 class Fenwick {
-	/*Fenwick tree for prefix sum query*/
-	private int[] nums; 
+    /*Fenwick tree for prefix sum query*/
+    private int[] nums; 
 
-	public Fenwick(int n) {
-		nums = new int[n+1]; 
-	}
+    public Fenwick(int n) {
+        nums = new int[n+1]; 
+    }
 
     /*Update tree upon adding delta to kth element.*/
-	public void add(int k, int delta) {
-		for (++k; k < nums.length; k += k & -k)
-			nums[k] += delta; 
-	}
+    public void add(int k, int delta) {
+        for (++k; k < nums.length; k += k & -k)
+            nums[k] += delta; 
+    }
 
     /*Return the prefix sum up to kth index (inclusive).*/
-	public long query(int k) {
-		long ans = 0; 
-		for (++k; k > 0; k -= k & -k) 
-			ans += nums[k]; 
-		return ans; 
-	}
+    public long query(int k) {
+        long ans = 0; 
+        for (++k; k > 0; k -= k & -k) 
+            ans += nums[k]; 
+        return ans; 
+    }
 }
