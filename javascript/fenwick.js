a992d2e459e82a46c61ef9a83352ba9e1823d4a8
Ye Gao <gaoyescm@amazon.com>
2024-07-29 13:20:22 -0400

    add fenwick tree

---
+++ javascript/fenwick.js
@@ -0,0 +1,19 @@
+/*A Fenwick tree or binary indexed tree (BIT) is a data structure that can efficiently update values and calculate prefix sums in an array of values.*/
+
+class Fenwick {
+    constructor(n) {
+        this.nums = Array(n+1).fill(0); 
+    }
+    
+    add(k, delta) {
+        for (++k; k < this.nums.length; k += k & -k)
+            this.nums[k] += delta; 
+    }
+    
+    query(k) {
+        let ans = 0; 
+        for (++k; k; k -= k & -k) 
+            ans += this.nums[k]; 
+        return ans; 
+    }
+}
