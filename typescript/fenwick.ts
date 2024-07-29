3465e3666d13990cc8dd7b6ddba6322c660e3c72
Ye Gao <gaoyescm@amazon.com>
2024-07-29 13:42:04 -0400

    add/update fenwick tree

--- typescript/fenwick.ts
+++ typescript/fenwick.ts
@@ -2,6 +2,8 @@
 efficiently update values and calculate prefix sums in an array of values.*/
 
 class Fenwick {
+    private nums: Array<number>; 
+    
     constructor(n) {
         this.nums = Array(n+1).fill(0); 
     }
