3420c30d74c810dc1e359330d89fbe4f314a44a8
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-08-25 17:04:27 -0400

    update binary exponentiation implementation

--- cpp/misc.h
+++ cpp/misc.h
@@ -1,10 +1,10 @@
-constexpr 
-auto pow(long base, int exp, int mod) noexcept {
-    /* Compute base^exp % mod via binary exponentiation */
+constpr
+auto pow(long x, int p, int mod) noexcept {
+    /* Compute x^p % mod via binary exponentiation */
     long ans = 1; 
-    for (; exp; exp >>= 1) {
-        if (exp & 1) ans = ans * base % mod; 
-        base = base * base % mod; 
+    for (; p; p >>= 1) {
+        if (p & 1) ans = ans * x % mod;
+        x = x * x % mod;
     }
     return ans; 
 }; 
