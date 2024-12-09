24e3ab62800c79067419b63db972a2e0d65d0c8c
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-12-08 23:18:23 -0500

    Refactor segtree

--- javascript/segtree.js
+++ javascript/segtree.js
@@ -1,21 +1,44 @@
+/**
+ * The segment tree is a data structure used for storing information about
+ * intervals or segments. It allows querying which of the stored segments
+ * contain a given point. A similar data structure is the interval tree.
+ * @class
+ * @constructor
+ * @public
+ */
 class SegTree {
     
     constructor(arr) {
         this.n = arr.length;
         this.tree = Array(4*this.n).fill(0);
-        this.build(arr, 0, 0, this.n);
+        this.#build(arr, 0, 0, this.n);
     }
 
-    build(arr, k, lo, hi) {
+    /**
+     * Builds the tree.
+     * @param {array} arr - the array based on which the tree is built.
+     * @param {number} k  - the index of the array representing the tree.
+     * @param {number} lo - the lower end of the array representing the tree.
+     * @param {number} hi - the higher end of the array representing the tree.
+     */
+    #build(arr, k, lo, hi) {
         if (lo+1 == hi) this.tree[k] = arr[lo];
         else {
             const mid = lo + Math.floor((hi-lo)/2);
-            this.build(arr, 2*k+1, lo, mid);
-            this.build(arr, 2*k+2, mid, hi);
+            this.#build(arr, 2*k+1, lo, mid);
+            this.#build(arr, 2*k+2, mid, hi);
             this.tree[k] = Math.min(this.tree[2*k+1], this.tree[2*k+2]);
         }
     }
 
+    /**
+     * Updates value at i to val.
+     * @param {number} i   - the index of the value to be updated.
+     * @param {number} val - the value to be updated to.
+     * @param {number} k   - the index of the array representing the tree.
+     * @param {number} lo  - the lower end of the array representing the tree.
+     * @param {number} hi  - the higher end of the array representing the tree.
+     */
     update(i, val, k=0, lo=0, hi=0) {
         if (hi == 0) hi = this.n;
         if (lo+1 == hi) this.tree[k] = val;
@@ -27,6 +50,15 @@ class SegTree {
         }
     }
 
+    /**
+     * Query the minimum in the range of [qlo, qhi).
+     * @param {number} qlo - the lower end of query range (inclusive).
+     * @param {number} qhi - the higher end of query range (exclusive).
+     * @param {number} k   - the index of the array representing the tree.
+     * @param {number} lo  - the lower end of the array representing the tree.
+     * @param {number} hi  - the higher end of the array representing the tree.
+     * @returns the minimum value in the range [qlo, qhi).
+     */
     query(qlo, qhi, k=0, lo=0, hi=0) {
         if (hi == 0) hi = this.n;
         if (qhi <= lo || hi <= qlo) return Infinity;
--- typescript/segtree.ts
+++ typescript/segtree.ts
@@ -1,3 +1,11 @@
+/**
+ * The segment tree is a data structure used for storing information about
+ * intervals or segments. It allows querying which of the stored segments
+ * contain a given point. A similar data structure is the interval tree.
+ * @class
+ * @constructor
+ * @public
+ */
 class SegTree {
     private n: number;
     private tree: number[];
@@ -5,19 +13,34 @@ class SegTree {
     constructor(arr: number[]) {
         this.n = arr.length;
         this.tree = Array(4*this.n).fill(0);
-        this.build(arr, 0, 0, this.n);
+        this.#build(arr, 0, 0, this.n);
     }
 
-    build(arr: number[], k: number, lo: number, hi: number): void {
+    /**
+     * Builds the tree.
+     * @param {array} arr - the array based on which the tree is built.
+     * @param {number} k  - the index of the array representing the tree.
+     * @param {number} lo - the lower end of the array representing the tree.
+     * @param {number} hi - the higher end of the array representing the tree.
+     */
+    #build(arr: number[], k: number, lo: number, hi: number): void {
         if (lo+1 == hi) this.tree[k] = arr[lo];
         else {
             const mid = lo + Math.floor((hi-lo)/2);
-            this.build(arr, 2*k+1, lo, mid);
-            this.build(arr, 2*k+2, mid, hi);
+            this.#build(arr, 2*k+1, lo, mid);
+            this.#build(arr, 2*k+2, mid, hi);
             this.tree[k] = Math.min(this.tree[2*k+1], this.tree[2*k+2]);
         }
     }
 
+    /**
+     * Updates value at i to val.
+     * @param {number} i   - the index of the value to be updated.
+     * @param {number} val - the value to be updated to.
+     * @param {number} k   - the index of the array representing the tree.
+     * @param {number} lo  - the lower end of the array representing the tree.
+     * @param {number} hi  - the higher end of the array representing the tree.
+     */
     update(i: number, val: number, k=0, lo=0, hi=0): void {
         if (hi == 0) hi = this.n;
         if (lo+1 == hi) this.tree[k] = val;
@@ -29,6 +52,15 @@ class SegTree {
         }
     }
 
+    /**
+     * Query the minimum in the range of [qlo, qhi).
+     * @param {number} qlo - the lower end of query range (inclusive).
+     * @param {number} qhi - the higher end of query range (exclusive).
+     * @param {number} k   - the index of the array representing the tree.
+     * @param {number} lo  - the lower end of the array representing the tree.
+     * @param {number} hi  - the higher end of the array representing the tree.
+     * @returns the minimum value in the range [qlo, qhi).
+     */
     query(qlo: number, qhi: number, k=0, lo=0, hi=0): number {
         if (hi == 0) hi = this.n;
         if (qhi <= lo || hi <= qlo) return Infinity;
